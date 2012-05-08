46340e0dbeb36222ea90eca4591fa0c780f1395a
Lim Chee Kin <limcheekin@vobject.com>
2012-05-08 22:00:11 +0800

    first import

---
+++ org.openl.tablets.tutorial1/src/org/openl/tablets/tutorial1/HelloDynamic.java
@@ -0,0 +1,6 @@
+package org.openl.tablets.tutorial1;
+
+public interface HelloDynamic {
+	void hello1(int i);
+	String hello2(int i);
+}
---
+++ org.openl.tablets.tutorial1/test/org/openl/tablets/tutorial1/Tutorial1DynamicTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.openl.tablets.tutorial1;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author root
+ *
+ */
+public class Tutorial1DynamicTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
